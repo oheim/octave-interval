c4bf805799e5e102e244638030f6472d0f687013
Oliver Heimlich <oheim@posteo.de>
2014-10-13 19:19:08 +0200

    Implemented reverse abs function

---
+++ inst/@infsup/absrev.m
@@ -0,0 +1,59 @@
+## Copyright 2014 Oliver Heimlich
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {Interval Function} {@var{X} =} absrev (@var{C}, @var{X})
+## @deftypefnx {Interval Function} {@var{X} =} absrev (@var{C})
+## @cindex IEEE1788 absRev
+## 
+## Compute the reverse absolute value function.
+##
+## Accuracy: The result is a tight enclosure.
+##
+## @example
+## @group
+## absrev (infsup (-2, 1))
+##   @result{} [-1, 1]
+## @end group
+## @end example
+## @seealso{abs}
+## @end deftypefn
+
+## Author: Oliver Heimlich
+## Keywords: interval
+## Created: 2014-10-13
+
+function result = absrev (c, x)
+
+if (nargin < 2)
+    x = infsup (-inf, inf);
+endif
+
+## Convert first parameter into interval, if necessary
+if (not (isa (c, "infsup")))
+    c = infsup (c);
+endif
+
+## Convert second parameter into interval, if necessary
+if (not (isa (x, "infsup")))
+    x = infsup (x);
+endif
+
+p = c & infsup (0, inf);
+n = - p;
+
+result = (p & x) | (n & x);
+
+endfunction
