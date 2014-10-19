a0ffd63a8b086cd3ba3a0dda30ce104069adeffa
Oliver Heimlich <oheim@posteo.de>
2014-10-19 18:19:45 +0200

    Implemented Two-output division

---
+++ inst/@infsupdec/divtopair.m
@@ -0,0 +1,76 @@
+## Copyright 2014 Oliver Heimlich
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {Interval Function} {[@var{U}, @var{V} =} divtopair (@var{X}, @var{Y})
+## @cindex IEEE1788 divToPair
+## 
+## Divide all numbers of interval @var{X} by all numbers of @var{Y}.  If the 
+## set division of the intervals would be a union of two disjoint intervals,
+## this function returns an enclosure of both intervals separately.
+##
+## Accuracy: The result is a tight enclosure.
+##
+## @example
+## @group
+## x = infsupdec (1);
+## y = infsupdec (-inf, inf);
+## [u, v] = divtopair (x, y)
+##   @result{} [-Inf, 0]_trv
+##   @result{} [0, Inf]_trv
+## @end group
+## @end example
+## @seealso{mrdivide}
+## @end deftypefn
+
+## Author: Oliver Heimlich
+## Keywords: interval
+## Created: 2014-10-19
+
+function [u, v] = divtopair (x, y)
+
+assert (nargin == 2);
+
+## Convert first parameter into interval, if necessary
+if (not (isa (x, "infsupdec")))
+    x = infsupdec (x);
+endif
+
+## Convert divisor into interval, if necessary
+if (not (isa (y, "infsupdec")))
+    y = infsupdec (y);
+endif
+
+if (isnai (x))
+    result = x;
+    return
+endif
+
+if (isnai (y))
+    result = y;
+    return
+endif
+
+[u, v] = divtopair (intervalpart (x), intervalpart (y));
+if (ismember (0, y))
+    ## division by zero is undefined
+    u = decorateresult (u, {x, y}, "trv");
+    v = decorateresult (v, {x, y}, "trv");
+else
+    u = decorateresult (u, {x, y});
+    v = decorateresult (v, {x, y});
+endif
+
+endfunction
