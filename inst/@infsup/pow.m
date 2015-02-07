da036139a78381f42de79573285370405f82f7a0
Oliver Heimlich <oheim@posteo.de>
2015-02-07 22:10:06 +0100

    pow.m: Changed definition for x = 0

--- inst/@infsup/pow.m
+++ inst/@infsup/pow.m
@@ -20,7 +20,9 @@
 ## Compute the simple power function on intervals defined by 
 ## @code{exp (@var{Y} * log (@var{X}))}.
 ##
-## The function is only defined where @var{X} is positive, cf. log function.
+## The function is only defined where @var{X} is positive or where @var{X} is
+## zero and @var{Y} is positive.
+##
 ## A general power function is implemented by @code{power}.
 ##
 ## Accuracy: The result is a tight enclosure.
@@ -51,7 +53,19 @@ if (not (isa (y, "infsup")))
     y = infsup (y);
 endif
 
+## Resize, if scalar × matrix
+if (isscalar (x.inf) ~= isscalar (y.inf))
+    x.inf = ones (size (y.inf)) .* x.inf;
+    x.sup = ones (size (y.inf)) .* x.sup;
+    y.inf = ones (size (x.inf)) .* y.inf;
+    y.sup = ones (size (x.inf)) .* y.sup;
+endif
+
+## Intersect with domain
 x = x & infsup (0, inf);
+y.inf (x.sup == 0) = max (0, y.inf (x.sup == 0));
+y.sup (y.inf > y.sup) = -inf;
+y.inf (y.inf > y.sup) = inf;
 
 ## Simple cases with no limit values, see Table 3.3 in
 ## Heimlich, Oliver. 2011. “The General Interval Power Function.”
@@ -76,9 +90,12 @@ u = max (max (max (...
          mpfr_function_d ('pow', +inf, x.sup, y.inf)), ...
          mpfr_function_d ('pow', +inf, x.sup, y.sup));
 
-emptyresult = isempty (x) | isempty (y) | x.sup == 0;
+emptyresult = isempty (x) | isempty (y) | (x.sup == 0 & y.sup == 0);
 l (emptyresult) = inf;
 u (emptyresult) = -inf;
+
+## Fix 0 ^ positive = 0
+u (x.sup == 0 && u == 1) = 0;
 
 result = infsup (l, u);
 
