81078b6bd38c6b51a15ebbcdf0b88f8c95a3f94d
Oliver Heimlich <oheim@posteo.de>
2015-02-21 12:37:35 +0100

    mtimes.m: Implemented decorated variant (after changing the mtimes algorithm)

--- inst/@infsupdec/mtimes.m
+++ inst/@infsupdec/mtimes.m
@@ -1,4 +1,4 @@
-## Copyright 2014-2015 Oliver Heimlich
+## Copyright 2015 Oliver Heimlich
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -24,53 +24,51 @@
 ##
 ## @example
 ## @group
-## x = infsup ([1, 2; 7, 15], [2, 2; 7.5, 15]);
-## y = infsup ([3, 3; 0, 1], [3, 3.25; 0, 2]);
+## x = infsupdec ([1, 2; 7, 15], [2, 2; 7.5, 15]);
+## y = infsupdec ([3, 3; 0, 1], [3, 3.25; 0, 2]);
 ## x * y
 ##   @result{} 2×2 interval matrix
-##          [3, 6]      [5, 10.5]
-##      [21, 22.5]   [36, 54.375]
+##          [3, 6]_com      [5, 10.5]_com
+##      [21, 22.5]_com   [36, 54.375]_com
 ## @end group
 ## @end example
-## @seealso{@@infsup/mrdivide}
+## @seealso{@@infsupdec/mrdivide}
 ## @end deftypefn
 
 ## Author: Oliver Heimlich
 ## Keywords: interval
-## Created: 2014-10-31
+## Created: 2015-02-21
 
 function result = mtimes (x, y)
 
-if (nargin < 2)
+if (nargin ~= 2)
     print_usage ();
     return
 endif
-if (not (isa (x, "infsup")))
-    x = infsup (x);
+
+if (not (isa (x, "infsupdec")))
+    x = infsupdec (x);
 endif
-if (not (isa (y, "infsup")))
-    y = infsup (y);
+if (not (isa (y, "infsupdec")))
+    y = infsupdec (y);
 endif
 
-if (isscalar (x) || isscalar (y))
-    result = times (x, y);
+## null matrix input -> null matrix output
+if (isempty (x.dec) || isempty (y.dec))
+    result = infsupdec (zeros (0));
     return
 endif
 
-if (size (x.inf, 2) ~= size (y.inf, 1))
-    error ("interval:InvalidOperand", ...
-           "operator *: nonconformant arguments");
-endif
+result = infsupdec (mtimes (intervalpart (x), intervalpart (y)));
 
-## mtimes could also be computed with a for loop and the dot operation.
-## However, that would take significantly longer (loop + missing JIT-compiler,
-## several OCT-file calls), so we use an optimized
-## OCT-file for that particular operation.
-
-[l, u] = mpfr_matrix_mul_d (x.inf, y.inf, x.sup, y.sup);
-result = infsup (l, u);
+dec_x = reducedec (x.dec, 2);
+dec_y = reducedec (y.dec, 1);
+parfor i = 1 : rows (x.dec)
+    result.dec (:, i) = mindec (result.dec (:, i), dec_x, dec_y (i));
+endparfor
 
 endfunction
 
+%!assert (isequal (infsupdec ([1, 2; 7, 15], [2, 2; 7.5, 15], {"com", "def"; "dac", "com"}) * infsupdec ([3, 3; 0, 1], [3, 3.25; 0, 2]), infsupdec ([3, 5; 21, 36], [6, 10.5; 22.5, 54.375], {"def", "def"; "dac", "dac"})));
 %!test "from the documentation string";
-%! assert (infsup ([1, 2; 7, 15], [2, 2; 7.5, 15]) * infsup ([3, 3; 0, 1], [3, 3.25; 0, 2]) == infsup ([3, 5; 21, 36], [6, 10.5; 22.5, 54.375]));
+%! assert (isequal (infsupdec ([1, 2; 7, 15], [2, 2; 7.5, 15]) * infsupdec ([3, 3; 0, 1], [3, 3.25; 0, 2]), infsupdec ([3, 5; 21, 36], [6, 10.5; 22.5, 54.375])));
