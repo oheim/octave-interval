aa18c16b23116e4a3757f2d4a965eb86c44808e6
Oliver Heimlich <oheim@posteo.de>
2015-05-30 21:42:50 +0200

    maint: newdec.m: Added missing encoding information

--- inst/@infsup/newdec.m
+++ inst/@infsup/newdec.m
@@ -14,6 +14,7 @@
 ## along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
+## @documentencoding UTF-8
 ## @deftypefn {Function File} {} newdec (@var{X})
 ## 
 ## Create a decorated interval from a bare interval.
