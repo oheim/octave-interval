8a8fd9601e428e16df42f24f18db96c85dc4f04c
Oliver Heimlich <oheim@posteo.de>
2015-05-30 15:05:05 +0200

    maint: bisect.m: Fixed encoding information for documentation string

--- inst/@infsup/bisect.m
+++ inst/@infsup/bisect.m
@@ -14,7 +14,7 @@
 ## along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @documentencoding utf-8
+## @documentencoding UTF-8
 ## @deftypefn {Function File} {[@var{A}, @var{B}] =} bisect (@var{X})
 ##
 ## Bisect an interval into two intervals, which contain half the amount of
