0bd6d8722344fca6941340251b4f37f28f882171
Oliver Heimlich <oheim@posteo.de>
2015-06-28 01:00:27 +0200

    plot3: Fixed indexing for interval matrices with different kinds of intervals

--- doc/image/sombrero.m
+++ doc/image/sombrero.m
@@ -17,24 +17,28 @@ range = linspace (-8, 8, 13);
 x = midrad (vec (x), (range (2) - range (1)) ./ 2);
 y = midrad (vec (y), (range (2) - range (1)) ./ 2);
 
-z = sin (hypot (x, y)) ./ hypot (x, y);
+h = hypot (x, y);
+z = sin (h) ./ h;
+## The sombrero function would suffer from the dependency problem
+## Thus, we compute an interval version of the function, which works around it.
+select = mag (h) < pi;
+z (select) = hull (sin (mag (h (select))) ./ mag (h (select)), ...
+                   sin (mig (h (select))) ./ mig (h (select)));
+z (mig (h) == 0) = union (z (mig (h) == 0), 1);
 
 flat = sup (z) < .3;
-low = ismember (sup (z), infsup (0.3, 1));
-mid = ismember (sup (z), infsup (1, 1.2));
-high = ismember (sup (z), infsup (1.2, 3));
-unbound = sup (z) > 2;
-
-## Infinite patches can't be displayed
-z = intersect (z, infsupdec (-10, 10));
+low = ismember (sup (z), infsup (0.3, 0.8));
+mid = ismember (sup (z), infsup (0.8, 0.9));
+high = ismember (sup (z), infsup (0.9, 1));
+top = sup (z) >= 1;
 
 plot3 (x (flat), y (flat), z (flat), blue, light)
 plot3 (x (low), y (low), z (low), green, light)
 plot3 (x (mid), y (mid), z (mid), yellow, light)
 plot3 (x (high), y (high), z (high), orange, light)
-plot3 (x (unbound), y (unbound), z (unbound), red, light)
+plot3 (x (top), y (top), z (top), red, light)
 
 view ([-37.5, 30])
-zlim ([-.5, 2])
+zlim ([-.4, 1])
 box off
 set (gca, "xgrid", "on", "ygrid", "on", "zgrid", "on")
