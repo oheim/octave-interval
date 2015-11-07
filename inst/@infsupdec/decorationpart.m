adee75e33bbae0d622cf783988e43b5808e1c2e6
Oliver Heimlich <oheim@posteo.de>
2015-11-07 17:13:31 +0100

    decorationpart.m: Added second parameter TYPE for return of numeric values

--- inst/@infsupdec/decorationpart.m
+++ inst/@infsupdec/decorationpart.m
@@ -16,12 +16,41 @@
 ## -*- texinfo -*-
 ## @documentencoding UTF-8
 ## @deftypefn {Function File} {} decorationpart (@var{X})
+## @deftypefnx {Function File} {} decorationpart (@var{X}, @var{TYPE})
 ## 
 ## Return the decoration of the decorated interval @var{X}.
 ##
+## The returned value depends on the requested @var{TYPE}.
+## @table @asis
+## @item @option{char} (default)
 ## The decoration is a cell array of character strings and its size equals the
-## size of its intervalpart.
+## size of its interval part.  Possible values: @option{ill}, @option{trv},
+## @option{def}, @option{dac}, @option{com}.
+## @item @option{uint8}
+## The decoration is a matrix of uint8 numbers and its size equals the size of
+## its interval part.  Possible values: @option{0}, @option{4}, @option{8}, 
+## @option{12}, @option{16} as defined by IEEE Std 1788-2015, 14.4 Interchange
+## representations and encodings.
+## @end table
 ##
+## @example
+## @group
+## x = infsupdec ("[0, 1] [1, inf] [-1, 1]_def [empty]");
+## decorationpart (x)
+##   @result{} ans =
+##     @{
+##       [1,1] = com
+##       [1,2] = dac
+##       [1,3] = def
+##       [1,4] = trv
+##     @}
+## decorationpart (x, "uint8")
+##   @result{} ans =
+##
+##       16  12   8   4
+##
+## @end group
+## @end example
 ## @seealso{@@infsupdec/intervalpart}
 ## @end deftypefn
 
@@ -29,18 +58,37 @@
 ## Keywords: interval
 ## Created: 2014-10-12
 
-function dec = decorationpart (x)
+function dec = decorationpart (x, type)
 
-if (nargin ~= 1)
+if (nargin > 2)
     print_usage ();
     return
 endif
 
+if (nargin < 2)
+    type = "char";
+endif
+
+switch (type)
+    case "char"
+        # see below
+    case "uint8"
+        dec = x.dec;
+        return
+    otherwise
+        error ("decorationpart: illegal value for parameter TYPE")
+endswitch
+
 persistent dec_translation = {...
+    # 0 = ill
     "ill", [], [], [], ...
+    # 4 = trv
     "trv", [], [], [], ...
+    # 8 = def
     "def", [], [], [], ...
+    # 12 = dac
     "dac", [], [], [], ...
+    # 16 = com
     "com"};
 
 dec = cell (size (x.dec));
