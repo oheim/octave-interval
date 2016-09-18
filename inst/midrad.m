db6c762b9a03de86ccc88f11c3795b9263146414
Oliver Heimlich <oheim@posteo.de>
2016-09-18 13:05:22 +0200

    midrad: Return midpoint and radius for intervals (2 arg out)
    * Fixed function to work with new constructors
    * Added second call syntax with two output arguments

--- inst/midrad.m
+++ inst/midrad.m
@@ -15,11 +15,15 @@
 
 ## -*- texinfo -*-
 ## @documentencoding UTF-8
-## @defun midrad (@var{M}, @var{R})
-## @defunx midrad (@var{M})
-## @defunx midrad ()
+## @deftypefun {@var{I} =} midrad (@var{M}, @var{R})
+## @deftypefunx {@var{I} =} midrad (@var{M})
+## @deftypefunx {@var{I} =} midrad ()
+## @deftypefunx {[@var{M}, @var{R}] =} midrad (@var{I})
 ## 
-## Create an interval enclosure for [@var{M}-@var{R}, @var{M}+@var{R}].
+## Create an interval enclosure @var{I} for [@var{M}-@var{R}, @var{M}+@var{R}].
+##
+## With two output arguments, compute a rigorous midpoint @var{M} and
+## radius @var{R} for interval @var{I}.
 ##
 ## Without input parameters, return the empty interval.  With only one input
 ## parameter, the radius @var{R} defaults to zero.
@@ -33,8 +37,15 @@
 ## with interval literals in uncertain form @code{m?ruE} can instead be used to
 ## create tight enclosures of decimal numbers with a radius.
 ##
-## Accuracy: The result is an accurate enclosure.  The result is tightest if
-## @var{M} and @var{R} are floating-point numbers or intervals.
+## Accuracy (with one output argument): The result is an accurate enclosure.
+## The result is tightest if @var{M} and @var{R} are floating-point numbers or
+## intervals.
+##
+## Accuracy (with two output arguments): @var{M} is the interval's midpoint
+## in binary64 precision, rounded to nearest and ties to even.  The returned
+## radius @var{R} will make a tight enclosure of the interval together with
+## @var{M}.  That is, @var{R} is the smallest binary64 number, which will make
+## [@var{M}-@var{R}, @var{M}+@var{R}] enclose the interval @var{I}.
 ##
 ## @example
 ## @group
@@ -55,58 +66,72 @@
 ## Keywords: interval
 ## Created: 2015-03-06
 
-function result = midrad (m, r)
+function [m, r] = midrad (m, r)
+
+if (nargout > 1)
+    warning ("off", "id=interval:ImplicitPromote", "local");
+endif
 
 switch nargin
     case 0
-        result = infsupdec ();
+        i = infsupdec ();
+    
     case 1
-        result = infsupdec (m);
+        if (nargout == 2 && isa (m, "infsup"))
+            i = m;
+        else
+            i = infsupdec (m);
+        endif
+    
     case 2
         if (isfloat (m) && isreal (m) && ...
             isfloat (r) && isreal (r))
             ## Simple case: m and r are binary64 numbers
             l = mpfr_function_d ('minus', -inf, m, r);
             u = mpfr_function_d ('plus', +inf, m, r);
-            emptyresult = r < 0 & true (size (m));
-            l (emptyresult) = inf;
-            u (emptyresult) = -inf;
-            result = infsupdec (l, u);
+            i = infsupdec (l, u);
         else
             ## Complicated case: m and r are strings or other types
             m = infsupdec (m);
-            if (isnai (m))
-                result = m;
-                return
-            endif
             if (not (isa (r, "infsup")))
                 ## [-inf, r] should make a valid interval, unless r == -inf
                 ## Intersection with non-negative numbers ensures that we
                 ## return [Empty] if r < 0.
                 if (isfloat (r))
-                    r (r == -inf) = nan;
+                    r(r == -inf) = nan;
                 endif
                 r = intersect (infsupdec (-inf, r), infsupdec (0, inf));
-                if (not (isnai (r)))
-                    ## Fix decoration
-                    r = newdec (intervalpart (r));
-                endif
+                ## Fix decoration, since intersect would return “trv” at best.
+                legal_radius = not (isempty (r));
+                r(legal_radius) = newdec (intervalpart (r(legal_radius)));
             endif
             if (isa (r, "infsupdec"))
-                if (isnai (r))
-                    result = r;
-                    return
-                endif
                 dec_r = decorationpart (r);
             else
-                dec_r = "com";
+                dec_r = {"com"};
             endif
             sup_r = sup (r);
-            sup_r (sup_r < 0) = -inf;
+            sup_r(sup_r < 0) = -inf;
             r = infsupdec (-sup_r, sup_r, dec_r);
             
-            result = m + r;
+            i = m + r;
         endif
+        
+    otherwise
+        print_usage ();
+endswitch
+
+switch nargout
+    case {0, 1}
+        m = i;
+        
+    case 2
+        m = mid (i);
+        ## The midpoint is rounded to nearest and the radius
+        ## must cover both boundaries
+        r1 = mpfr_function_d ('minus', +inf, m, inf (i));
+        r2 = mpfr_function_d ('minus', +inf, sup (i), m);
+        r = max (r1, r2);
         
     otherwise
         print_usage ();
@@ -115,22 +140,29 @@ endswitch
 endfunction
 
 %!assert (isempty (midrad ()));
-%!assert (isempty (midrad (0, -inf)));
-%!assert (isempty (midrad (0, -.1)));
-%!assert (isempty (midrad (0, "-.1")));
-%!assert (isempty (midrad (0, infsup("-.1"))));
+%!warning id=interval:UndefinedOperation
+%! assert (isnai (midrad (0, -inf)));
+%!warning id=interval:UndefinedOperation
+%! assert (isnai (midrad (0, -.1)));
+%!warning id=interval:UndefinedOperation
+%! assert (isnai (midrad (0, "-.1")));
+%!warning id=interval:UndefinedOperation
+%! assert (isnai (midrad (0, infsup("-.1"))));
 %!assert (isequal (midrad ("pi"), infsupdec ("pi")));
-%!warning assert (isequal (midrad (infsup (2), 2), infsupdec (0, 4)));
+%!warning id=interval:ImplicitPromote
+%! assert (isequal (midrad (infsup (2), 2), infsupdec (0, 4)));
 %!assert (isequal (midrad (2, infsup (2)), infsupdec (0, 4)));
-%!warning assert (isequal (midrad (infsup (2), infsup (2)), infsupdec (0, 4)));
+%!warning id=interval:ImplicitPromote
+%! assert (isequal (midrad (infsup (2), infsup (2)), infsupdec (0, 4)));
 %!assert (isequal (midrad (2, infsupdec (2)), infsupdec (0, 4)));
 %!assert (isequal (midrad (infsupdec (2), 2), infsupdec (0, 4)));
-%!warning assert (isequal (midrad (infsup (2), infsupdec (2)), infsupdec (0, 4)));
+%!warning id=interval:ImplicitPromote
+%! assert (isequal (midrad (infsup (2), infsupdec (2)), infsupdec (0, 4)));
 %!assert (isequal (midrad (infsupdec (2), infsup (2)), infsupdec (0, 4)));
 %!assert (isequal (midrad (infsupdec (2), infsupdec (2)), infsupdec (0, 4)));
 %!assert (isequal (midrad (1, magic (3)), infsupdec ([-7, 0, -5; -2, -4, -6; -3, -8, -1], [9, 2, 7; 4, 6, 8; 5, 10, 3])));
 %!assert (isequal (midrad (magic (3), 1), infsupdec ([7, 0, 5; 2, 4, 6; 3, 8, 1], [9, 2, 7; 4, 6, 8; 5, 10, 3])));
-%!test "from the documentation string";
-%! assert (isequal (midrad (42, 3), infsupdec (39, 45)));
-%! assert (isequal (midrad (0, inf), entire ()));
-%! assert (isequal (midrad ("1.1", "0.1"), infsupdec (1 - eps, "1.2")));
+%!# from the documentation string
+%!assert (isequal (midrad (42, 3), infsupdec (39, 45)));
+%!assert (isequal (midrad (0, inf), entire ()));
+%!assert (isequal (midrad ("1.1", "0.1"), infsupdec (1 - eps, "1.2")));
