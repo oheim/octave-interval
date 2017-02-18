bf6c197a2a6c1cc404902a3938344357826e2031
Oliver Heimlich <oheim@posteo.de>
2017-02-18 17:39:40 +0100

    doc: sqrrev: Mention of former sqr function removed

--- inst/@infsupdec/sqrrev.m
+++ inst/@infsupdec/sqrrev.m
@@ -21,7 +21,7 @@
 ## Compute the reverse square function.
 ##
 ## That is, an enclosure of all @code{x ∈ @var{X}} where
-## @code{sqr (x) ∈ @var{C}}.
+## @code{x .^ 2 ∈ @var{C}}.
 ##
 ## Accuracy: The result is a tight enclosure.
 ##
