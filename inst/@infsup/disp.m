607423f77a20e46b7e0cb865fb8d29372b224f76
Joel Dahne <joeldahne@hotmail.com>
2017-06-12 09:54:36 +0200

    @infsup/disp.m: Fixed coding style

--- inst/@infsup/disp.m
+++ inst/@infsup/disp.m
@@ -53,76 +53,77 @@
 
 function varargout = disp (x)
 
-if (nargin ~= 1)
+  if (nargin ~= 1)
     print_usage ();
     return
-endif
+  endif
 
-## With format="auto" the output precision can be set with the format command
-[s, isexact] = intervaltotext (x, "auto");
+  ## With format="auto" the output precision can be set with the format command
+  [s, isexact] = intervaltotext (x, "auto");
 
-if (not (iscell (s)))
+  if (not (iscell (s)))
     ## Scalar interval
     if (nargout == 0)
-        disp (s);
+      disp (s);
     else
-        varargout{1} = cstrcat (s, "\n");
-        varargout{2} = isexact;
+      varargout{1} = cstrcat (s, "\n");
+      varargout{2} = isexact;
     endif
     return
-endif
+  endif
 
-## Interval matrix / vector
-columnwidth = max (cellfun ("length", s), [], 1);
-columnwidth += 3; # add 3 spaces between columns
+  ## Interval matrix / vector
+  columnwidth = max (cellfun ("length", s), [], 1);
+  columnwidth += 3; # add 3 spaces between columns
 
-## Print all columns
-buffer = "";
-if (rows (x.inf) > 0)
+  ## Print all columns
+  buffer = "";
+  if (rows (x.inf) > 0)
     ## FIXME: See display.m for how current_print_indent_level is used
     global current_print_indent_level;
     maxwidth = terminal_size ()(2) - current_print_indent_level;
     cstart = uint32 (1);
     cend = cstart - 1;
     while (cstart <= columns (x.inf))
-        ## Determine number of columns to print, print at least one column
-        usedwidth = 0;
-        submatrix = "";
-        do
-            cend ++;
-            submatrix = strcat (submatrix, ...
-                prepad (strjust (char (s(:, cend))), columnwidth(cend), " ", 2));
-            usedwidth += columnwidth(cend);
-        until (cend == columns (x.inf) || ...
-               (split_long_rows () && ...
-                 usedwidth + columnwidth(cend + 1) > maxwidth))
-        if (cstart > 1 || cend < columns (x.inf))
-            if (cstart > 1)
-                buffer = cstrcat (buffer, "\n");
-            endif
-            if (cend > cstart)
-                buffer = cstrcat (buffer, ...
-                                  sprintf(" Columns %d through %d:\n\n", ...
-                                        cstart, cend)); ...
-            else
-                buffer = cstrcat (buffer, ...
-                                  sprintf(" Column %d:\n\n", cstart));
-            endif
+      ## Determine number of columns to print, print at least one column
+      usedwidth = 0;
+      submatrix = "";
+      do
+        cend ++;
+        submatrix = strcat (submatrix, ...
+                            prepad (strjust (char (s(:, cend))), ...
+                                    columnwidth(cend), " ", 2));
+        usedwidth += columnwidth(cend);
+      until (cend == columns (x.inf) || ...
+             (split_long_rows () && ...
+              usedwidth + columnwidth(cend + 1) > maxwidth))
+      if (cstart > 1 || cend < columns (x.inf))
+        if (cstart > 1)
+          buffer = cstrcat (buffer, "\n");
         endif
-        ## Convert string matrix into string with newlines
-        buffer = cstrcat (buffer, strjoin (cellstr (submatrix), "\n"), "\n");
-        if (nargout == 0)
-            printf (buffer);
-            buffer = "";
+        if (cend > cstart)
+          buffer = cstrcat (buffer, ...
+                            sprintf(" Columns %d through %d:\n\n", ...
+                                    cstart, cend)); ...
+        else
+          buffer = cstrcat (buffer, ...
+                            sprintf(" Column %d:\n\n", cstart));
         endif
-        cstart = cend + 1;
+      endif
+      ## Convert string matrix into string with newlines
+      buffer = cstrcat (buffer, strjoin (cellstr (submatrix), "\n"), "\n");
+      if (nargout == 0)
+        printf (buffer);
+        buffer = "";
+      endif
+      cstart = cend + 1;
     endwhile
-endif
+  endif
 
-if (nargout > 0)
+  if (nargout > 0)
     varargout{1} = buffer;
     varargout{2} = isexact;
-endif
+  endif
 
 endfunction
 
