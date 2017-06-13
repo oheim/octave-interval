7c01409de8fcd4a0d60b0cf2f321516509833474
Joel Dahne <joeldahne@hotmail.com>
2017-06-13 08:59:18 +0200

    hull.m: Added support for N-dimensional arrays

--- inst/hull.m
+++ inst/hull.m
@@ -16,7 +16,7 @@
 ## -*- texinfo -*-
 ## @documentencoding UTF-8
 ## @defun hull (@var{X1}, @var{X2}, …)
-## 
+##
 ## Create an interval enclosure for a list of parameters.
 ##
 ## Parameters can be simple numbers, intervals or interval literals as strings.
@@ -28,7 +28,7 @@
 ## will not be members of the interval.  In particular, it is not possible to
 ## create an interval with @code{hull (inf)}.
 ##
-## The result is equivalent to 
+## The result is equivalent to
 ## @code{union (infsupdec (@var{X1}), union (infsupdec (@var{X2}), …))}, but
 ## computed in a more efficient way.  In contrast to the union function, this
 ## function is not considered a set operation and the result carries the best
@@ -61,121 +61,116 @@
 
 function result = hull (varargin)
 
-if (isempty (varargin))
+  if (isempty (varargin))
     result = infsupdec ();
     return
-endif
+  endif
 
-l = u = cell (size (varargin));
+  l = u = cell (size (varargin));
 
-## Floating point numbers can be used without conversion
-floats = cellfun (@isfloat, varargin);
-l(floats) = u(floats) = varargin(floats);
+  ## Floating point numbers can be used without conversion
+  floats = cellfun (@isfloat, varargin);
+  l(floats) = u(floats) = varargin(floats);
 
-## Convert everything else to interval, if necessary
-decoratedintervals = cellfun ("isclass", varargin, "infsupdec");
-to_convert = not (decoratedintervals | floats);
-## Use infsupdec constructor for conversion, because it can handle decorated
-## interval literals.  Also, it will trigger an interval:ImplicitPromote
-## warning if necessary.
-varargin(to_convert) = cellfun (@infsupdec, varargin(to_convert), ...
-                                "UniformOutput", false ());
-decoratedintervals = not (floats);
+  ## Convert everything else to interval, if necessary
+  decoratedintervals = cellfun ("isclass", varargin, "infsupdec");
+  to_convert = not (decoratedintervals | floats);
+  ## Use infsupdec constructor for conversion, because it can handle decorated
+  ## interval literals.  Also, it will trigger an interval:ImplicitPromote
+  ## warning if necessary.
+  varargin(to_convert) = cellfun (@infsupdec, varargin(to_convert), ...
+                                  "UniformOutput", false ());
+  decoratedintervals = not (floats);
 
-## Extract inf and sup matrices for remaining elements of l and u.
-l(decoratedintervals) = cellfun (@inf, varargin(decoratedintervals), ...
-                                 "UniformOutput", false ());
-u(decoratedintervals) = cellfun (@sup, varargin(decoratedintervals), ...
-                                 "UniformOutput", false ());
+  ## Extract inf and sup matrices for remaining elements of l and u.
+  l(decoratedintervals) = cellfun (@inf, varargin(decoratedintervals), ...
+                                   "UniformOutput", false ());
+  u(decoratedintervals) = cellfun (@sup, varargin(decoratedintervals), ...
+                                   "UniformOutput", false ());
 
-## Broadcast nonsingleton dimensions (otherwise cat would throw an error below)
-sizes1 = cellfun ("size", l, 1);
-sizes2 = cellfun ("size", l, 2);
-targetsize = [max(sizes1) max(sizes2)];
-if ((targetsize(1) ~= 1 && min (sizes1 (sizes1 ~= 1)) ~= targetsize(1))
-    || (targetsize(2) ~= 1 && min (sizes2 (sizes2 ~= 1)) ~= targetsize(2)))
-    error ("hull: dimension mismatch")
-endif
-if (any (targetsize ~= [1 1]))
-    to_broadcast = sizes1 ~= targetsize(1) ...
-                 | sizes2 ~= targetsize(2);
-    if (any (to_broadcast))
-        for i = find (to_broadcast)
-            if (size (l{i}, 1) ~= targetsize(1))
-                ## Broadcast 1st dimension
-                l{i} = l{i} (ones (targetsize(1), 1), :);
-                u{i} = u{i} (ones (targetsize(1), 1), :);
-            endif
-            if (size (l{i}, 2) ~= targetsize(2))
-                ## Broadcast 2nd dimension
-                l{i} = l{i} (:, ones (targetsize(2), 1));
-                u{i} = u{i} (:, ones (targetsize(2), 1));
-            endif
-        endfor
+  ## Broadcast nonsingleton dimensions (otherwise cat would throw an error below)
+  numberofdimensions = max (cellfun ("ndims", l));
+  sizes = zeros(numberofdimensions, size (l)(2));
+  for dim = 1:numberofdimensions
+    sizes(dim, :) = cellfun("size", l, dim);
+  endfor
+  targetsize = max (sizes, [], 2);
+  if (!all (all (or (targetsize == sizes, sizes == 1))))
+    disp("hull: dimensions mismatch")
+  endif
+  for dim = 1:numberofdimensions
+    if (targetsize(dim) ~= 1)
+      to_broadcast = sizes(dim, :) == 1;
+      for i = find (to_broadcast)
+        broadcastvector = ones (numberofdimensions, 1);
+        broadcastvector(dim) = targetsize(dim);
+        l{i} = repmat (l{i}, broadcastvector);
+        u{i} = repmat (u{i}, broadcastvector);
+      endfor
     endif
-endif
+  endfor
 
-## Compute min and max of inf and sup matrices, NaNs would be ignored and must
-## be considered
-nans = false (targetsize);
-l = cat (3, l{:});
-nans(any (isnan (l), 3)) = true;
-l = min (l, [], 3);
-u = cat (3, u{:});
-nans(any (isnan (u), 3)) = true;
-u = max (u, [], 3);
+  ## Compute min and max of inf and sup matrices, NaNs would be ignored and must
+  ## be considered
+  nans = false (targetsize);
+  l = cat (numberofdimensions + 1, l{:});
+  nans(any (isnan (l), numberofdimensions + 1)) = true;
+  l = min (l, [], numberofdimensions + 1);
+  u = cat (numberofdimensions + 1, u{:});
+  nans(any (isnan (u), numberofdimensions + 1)) = true;
+  u = max (u, [], numberofdimensions + 1);
 
-## Compute best possible decoration
-dec = cell (targetsize);
-dec(:) = "com";
-dec(not (isfinite (l) & isfinite (u))) = "dac";
-dec(nans) = "trv";
+  ## Compute best possible decoration
+  dec = cell (targetsize);
+  dec(:) = "com";
+  dec(not (isfinite (l) & isfinite (u))) = "dac";
+  dec(nans) = "trv";
 
-## Consider input decorations
-if (any (decoratedintervals))
+  ## Consider input decorations
+  if (any (decoratedintervals))
     dec = mindec (dec, cellfun (@decorationpart, ...
                                 varargin(decoratedintervals), ...
                                 "UniformOutput", false ()));
-endif
+  endif
 
-nairesult = strcmp (dec, "ill");
-emptyresult = isnan (l) | isnan (u) | l > u | nairesult;
-l(emptyresult) = u(emptyresult) = 0;
-dec(nairesult) = "com";
-result = infsupdec (l, u, dec);
-result(emptyresult) = empty ();
-result(nairesult) = nai ();
+  nairesult = strcmp (dec, "ill");
+  emptyresult = isnan (l) | isnan (u) | l > u | nairesult;
+  l(emptyresult) = u(emptyresult) = 0;
+  dec(nairesult) = "com";
+  result = infsupdec (l, u, dec);
+  result(emptyresult) = empty ();
+  result(nairesult) = nai ();
 
 endfunction
 
 function decoration = mindec (decoration, decorations)
 
-## Determine and apply the minimum decoration
-for i = 1 : length (decorations)
+  ## Determine and apply the minimum decoration
+  for i = 1 : length (decorations)
     if (iscell (decorations{i}) && not (isempty (decorations{i})))
-        otherdecoration = decorations{i}{1};
+      otherdecoration = decorations{i}{1};
     else
-        otherdecoration = decorations{i};
+      otherdecoration = decorations{i};
     endif
 
     ## Only check distinct elements
     for n = find (not (strcmp (decoration, decorations{i})))(:)'
-        if (iscell (decorations{i}) && not (isscalar (decorations{i})))
-            otherdecoration = decorations{i}{n};
-        else
-            ## Scalars broadcast into the whole cell array. The value is set
-            ## once before the inner for loop.
-        endif
+      if (iscell (decorations{i}) && not (isscalar (decorations{i})))
+        otherdecoration = decorations{i}{n};
+      else
+        ## Scalars broadcast into the whole cell array. The value is set
+        ## once before the inner for loop.
+      endif
 
-        ## Because of the simple propagation order com > dac > def > trv, we
-        ## can use string comparison order.
-        if (!strcmp (decoration{n}, "ill") && ...
-            (strcmp (otherdecoration, "ill") || ...
-                sign ((decoration{n}) - otherdecoration) * [4; 2; 1] < 0))
-            decoration{n} = otherdecoration;
-        endif
+      ## Because of the simple propagation order com > dac > def > trv, we
+      ## can use string comparison order.
+      if (!strcmp (decoration{n}, "ill") && ...
+          (strcmp (otherdecoration, "ill") || ...
+           sign ((decoration{n}) - otherdecoration) * [4; 2; 1] < 0))
+        decoration{n} = otherdecoration;
+      endif
     endfor
-endfor
+  endfor
 
 endfunction
 
@@ -189,6 +184,9 @@ endfunction
 %!assert (isnai (hull ([nai, 2], [nai, 3])), logical ([1 0]));
 %!assert (isnai (hull ([nai, 2], nai)), logical ([1 1]));
 %!assert (isnai (hull ([nai, 2], [2, nai])), logical ([1 1]));
+%!assert (isequal (hull (zeros (2, 2, 2, 2), ones (2, 2, 2, 2)), infsupdec (zeros (2, 2, 2, 2), ones (2, 2, 2, 2))))
+%!assert (isequal (hull (zeros (2, 1, 4, 1), ones (1, 3, 1, 5), -1), infsupdec (-ones (2, 3, 4, 5), ones (2, 3, 4, 5))))
+%!assert (isnai (hull (zeros (2, 2, 2, 2), ones (2, 2, 2, 2), nai)), logical (ones (2, 2, 2, 2)))
 
 %!test "from the documentation string";
 %! assert (isequal (hull (1, 2, 3, 4), infsupdec (1, 4, "com")));
