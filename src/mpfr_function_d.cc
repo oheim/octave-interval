478f17f63f74f94aece6f423a9061e85ca44d6de
Oliver Heimlich <oheim@posteo.de>
2017-07-04 23:32:32 +0200

    maint: mpfr_function_d.cc: Add copyright notice for sources of inspirations for broadcasting operations in ND arrays

--- src/mpfr_function_d.cc
+++ src/mpfr_function_d.cc
@@ -1,6 +1,8 @@
 /*
   Copyright 2015-2016 Oliver Heimlich
   Copyright 2017 Joel Dahne
+  Copyright 2009-2017 Jaroslav Hajek
+  Copyright 2009 VZLU Prague  
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
