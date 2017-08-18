cb96a3fea8d9e0286ade5bc332554a57f8f66d17
Oliver Heimlich <oheim@posteo.de>
2017-08-18 19:49:05 +0200

    maint: Add copyright notice to functions modified since last release  * GSoC by Joel  * Updated copyright year for my own modifications

--- src/mpfr_matrix_sqr_d.cc
+++ src/mpfr_matrix_sqr_d.cc
@@ -1,5 +1,5 @@
 /*
-  Copyright 2016 Oliver Heimlich
+  Copyright 2016-2017 Oliver Heimlich
   
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
